9e84f52fa9f5e50dc364c24fabd984ff6110de3a
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-06-20 22:37:56 -0600

    reorganized function
    functions in alphabetical order.
    Added tests at bottom

--- practice_functions.py
+++ practice_functions.py
@@ -1,21 +1,12 @@
 def find_square_root(x):
     i = 0
-    while i <= x//2:
+    while i <= x // 2:
         if i * i == x:
             return i
         i += 1
     return -1
 
 
-def product_by_addition(a, b):
-    if a == 0:
-        return 0
-
-    return b + product_by_addition(a - 1, b)
-
-
-# sorry andrew ik this is not the fastest
-# but proving a sieve is hard T_T
 def is_prime(x):
     if 2 >= x > 0:
         return True
@@ -28,6 +19,17 @@ def is_prime(x):
     return True
 
 
+def product_by_addition(a, b):
+    if a == 0:
+        return 0
+
+    return b + product_by_addition(a - 1, b)
+
+
+# sorry andrew ik this is not the fastest
+# but proving a sieve is hard T_T
+
+
 def rev_bubble_sort(A):
     for i in range(len(A)):
         for j in range(len(A) - 1):
@@ -37,3 +39,18 @@ def rev_bubble_sort(A):
                 A[j + 1] = temp
 
     return A
+
+
+print(find_square_root(10))
+print(find_square_root(9))
+
+print(is_prime(1))
+print(is_prime(2))
+print(is_prime(7))
+print(is_prime(12))
+print(is_prime(15))
+
+print(product_by_addition(0, 20))
+print(product_by_addition(10, 20))
+
+print(rev_bubble_sort([9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9]))
