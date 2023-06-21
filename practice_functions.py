cb4e6d82404a0b597de50ed047506170c3f5eeaa
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-06-20 21:18:56 -0600

    bubble sort added

--- practice_functions.py
+++ practice_functions.py
@@ -25,4 +25,13 @@ def is_prime(x):
         i += 1
     return True
 
-print(is_prime(2))
+
+def rev_bubble_sort(A):
+    for i in range(len(A)):
+        for j in range(len(A) - 1):
+            if A[j] > A[j + 1]:
+                temp = A[j]
+                A[j] = A[j + 1]
+                A[j + 1] = temp
+
+    return A
