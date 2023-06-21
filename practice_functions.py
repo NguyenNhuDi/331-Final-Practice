e9aa91c28d9ef95259976ebc7716cba11ecd3771
Di Nguyen <81270509+NguyenNhuDi@users.noreply.github.com>
2023-06-20 22:38:28 -0600

    made it look pretty

--- practice_functions.py
+++ practice_functions.py
@@ -53,4 +53,4 @@ print(is_prime(15))
 print(product_by_addition(0, 20))
 print(product_by_addition(10, 20))
 
-print(rev_bubble_sort([9,8,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,9]))
+print(rev_bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9]))
